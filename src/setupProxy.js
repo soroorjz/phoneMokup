165f960f133b090974bba9e4d21eed00bc60d7d7
SoroorJameizadeh <soroorjameizadeh@gmail.com>
2025-03-06 18:52:43 +0330

    api address changed

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     "/api",  // مسیر درخواست‌ها
     createProxyMiddleware({
-      target: "https://smp.devrayan.ir:2053", // سرور مقصد
+      target: "https://smp.devrayan.ir", // سرور مقصد
       changeOrigin: true,
       secure: false, // در صورت مشکل SSL
     })
